1bd83cb353548703212db2e78ec8ccd8c8013d53
Alexzhyliuk <49272304+Alexzhyliuk@users.noreply.github.com>
2019-06-24 11:42:36 +0300

    Add procfile

--- todoapp/todoapp/settings.py
+++ todoapp/todoapp/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -128,3 +129,5 @@ EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER", "alexandr-zhyliuk@mail.ru")
 EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD", "sasa8190221")
 EMAIL_PORT = int(os.environ.get("EMAIL_PORT", "587"))
 EMAIL_USE_TLS = os.environ.get("EMAIL_USE_TLS", 'false') != "True"
+
+django_heroku.settings(locals())
